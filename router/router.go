efd61cd9cd6da0074ce2323116452b48b7a15fa7
Baraffe Robin <robin.baraffe@gmail.com>
2020-11-27 11:02:20 +0100

    Update router.go

--- router/router.go
+++ router/router.go
@@ -16,7 +16,7 @@ var unProtectedRoutes = []string{"/swagger/"}
 // CreateRouter create notifications API routes
 func CreateRouter() {
 	router := mux.NewRouter()
-	APIRouter := router.PathPrefix("/api/v1").Subrouter()
+	APIRouter := router.PathPrefix("/v1").Subrouter()
 
 	APIRouter.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
