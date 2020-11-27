c8e44acd42b10336cb30e483719f24273b982c82
Thyrael <robin.baraffe@gmail.com>
2020-11-27 10:51:07 +0100

    Split middleware function into middlewares.go file

--- router/router.go
+++ router/router.go
@@ -1,22 +1,17 @@
 package router
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/qimpl/notifications/models"
-	"github.com/qimpl/notifications/services"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-var unProtectedRoutes = []string{}
+var unProtectedRoutes = []string{"/swagger/"}
 
 // CreateRouter create notifications API routes
 func CreateRouter() {
@@ -25,7 +20,9 @@ func CreateRouter() {
 
 	APIRouter.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
-	router.Use(jwtVerifyTokenMiddleware)
+	if os.Getenv("ENV") != "dev" {
+		router.Use(jwtTokenVerificationMiddleware)
+	}
 
 	createNotificationRouter(APIRouter)
 
@@ -44,38 +41,4 @@ func CreateRouter() {
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), handler); err != nil {
 		log.Print(err)
 	}
-}
-
-func jwtVerifyTokenMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		requestURI := strings.TrimPrefix(r.RequestURI, "/api/v1")
-		for _, route := range unProtectedRoutes {
-			if requestURI == route {
-				next.ServeHTTP(w, r)
-				return
-			}
-		}
-
-		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer")
-		if len(authHeader) != 2 {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			var badRequest *models.BadRequest
-			json.NewEncoder(w).Encode(badRequest.GetError("Malformed Authorization HTTP header"))
-
-			return
-		}
-
-		if _, err := services.ValidateJwtToken(authHeader[1]); err != nil {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusUnauthorized)
-			var Unauthorized *models.Unauthorized
-			json.NewEncoder(w).Encode(Unauthorized.GetError(fmt.Sprintf("Invalid jwt token : %s", err)))
-
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }
